4edff0b8846537209eb1e2c97cd57569bcc0723e
Bruno Jullien <bruno.jullien@gmail.com>
2023-02-03 21:34:19 -0800

    25. Configuring Spring Controllers

---
+++ src/main/java/com/bbj/spring6webapp/controllers/BookController.java
@@ -0,0 +1,32 @@
+package com.bbj.spring6webapp.controllers;
+
+import com.bbj.spring6webapp.services.BookService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+    //we use the interface, not the implementation (loosely coupled)
+    //so we will be able to use dependency injection flexibility, as BookService is a Spring Bean
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    /**
+     *
+     *
+     * @param model is a Spring MVC component, injected by Spring at runtime
+     * @return the view name
+     */
+    @RequestMapping("/books")                   //when Spring see this, /books queries will call getBooks(model)
+    public String getBooks(Model model) {
+
+        model.addAttribute("books", bookService.findAll());
+
+        return "books"; //view name, to be defined as Spring MVC view
+    }
+}
